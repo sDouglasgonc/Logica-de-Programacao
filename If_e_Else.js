d52d88266d6ac17997593ba76a24e8221727d252
sDouglasgonc <sdouglas968@gmail.com>
2023-03-02 20:11:24 -0300

    praticando

---
+++ If_e_Else.js
@@ -0,0 +1,11 @@
+//testando if e else
+
+var nome = "Douglas"
+var nacionalidade = "Brasileiro"
+
+
+if(nacionalidade != "Brasileiro" ){
+    console.log(" Errado!")
+}else{
+    console.log("O nome dele é "+nome+" e ele é "+nacionalidade)
+}
