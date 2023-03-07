1ccf43d664b2f8fcd906387470adfc0f3e93be21
sDouglasgonc <sdouglas968@gmail.com>
2023-03-07 20:49:47 -0300

    if e else ajeitado

--- If e Else/If_e_Else.js
+++ If e Else/If_e_Else.js
@@ -4,13 +4,14 @@ function soma(){
    var numero1 =  document.getElementById("num1")
    var numero2 = document.getElementById("num2")
    var res = document.getElementById("res")
-var n1 = Number(numero1.value)
-var n2 = Number(numero2.value)
-somar = n1 + n2
-if ( somar == 0 ) {
-window.alert("coloque numeros")
+
+
+if ( numero1.value.length == 0 && numero2.value.length == 0) {
+window.alert("ERRO: Não deixe campos vazios")
 } else {
-    
+    var n1 = Number(numero1.value)
+    var n2 = Number(numero2.value)
+    somar = n1 + n2
     res.innerHTML = `O resultado é ${somar}`
 }
     
