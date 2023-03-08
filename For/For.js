45d2c8ccbe071a0c56efd2a7ef5af96c2e237d2d
sDouglasgonc <sdouglas968@gmail.com>
2023-03-07 21:09:15 -0300

    tudo certo agr

--- For/For.js
+++ For/For.js
@@ -1,7 +1,7 @@
 function saibamais() {
-    const carro1 = {modelo:'Le-mans Sedan', marca:'Audi', ano: '2011'}
+    const carro1 = {Modelo:'Le mans <br>', Marca:'Audi <br>', Ano: '2011'}
     var show = document.getElementById("show")
 for(let carac in carro1){
- show.innerHTML = `${carac} : ${carro1[carac]}`
+ show.innerHTML += `${carac} : ${carro1[carac]}`
 }
 }
--- do while/do_e_while.js
+++ do while/do_e_while.js
@@ -1,11 +1,12 @@
 
 function contar(){
-    
+    let inicio = Number(document.getElementById("cont2").value)
     let terminador = Number(document.getElementById("cont").value)
-    let i = 1
+    let resultado = ''
     do{
-        document.getElementById("contagem").innerHTML = `Primeiro Passo:1 <br> E <br> Ultimo Passo:${i}`
-        i++
-    }while(i <= terminador  )
+        document.getElementById("contagem").innerHTML += `Passo:${resultado}<br>`
+        inicio += 1;
+       resultado = inicio + ' ';
+    }while(inicio <= terminador )
     window.alert("terminado")
 }
