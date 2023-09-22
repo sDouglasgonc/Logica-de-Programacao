736ae4f601e7bc7163be49ef8d8f7272bcc8484b
Douglas Gonçalo <sdouglas968@gmail.com>
2023-09-22 18:54:55 -0300

    desliga

--- testes JS/If_e_Else.js
+++ testes JS/If_e_Else.js
@@ -1,10 +1,4 @@
-function howMany(selectObject) {
-    var numeroSelecionadas = 0;
-    for (var i = 0; i < selectObject.options.length; i++) {
-        if (selectObject.options[i].selected) {
-            numeroSelecionadas++;
-        }
-    }
-    return numeroSelecionadas;
-}
-console.log(howMany(numeroSelecionadas))
+do {
+    i += 1;
+    console.log(i);
+} while (i < 5);
